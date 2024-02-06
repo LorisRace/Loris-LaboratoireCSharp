4e4ee8fd782450afe0a8dd86251329befcac4e63
LorisRace <145431646+LorisRace@users.noreply.github.com>
2024-02-06 11:03:58 +0100

    Initial Commit

---
+++ LorisLefevre-LaboratoireC#/Labo1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Labo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
